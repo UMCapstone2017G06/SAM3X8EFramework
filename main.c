99dcb3a6e0cc5e679bc81df20a91a7f261c94ba5
Brandon Hill <brandonhill7@gmail.com>
2017-10-27 14:54:18 -0500

    Main Commit

---
+++ main.c
@@ -0,0 +1,21 @@
+/*
+ * SAM3X8EFramework.c
+ *
+ * Created: 2017-10-27 2:52:28 PM
+ * Author : Brandon Hill
+ */ 
+
+
+#include "sam.h"
+
+
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
